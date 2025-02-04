ad3dc97ba7d18e8afbcc40b73c7b16f35399635f
JackAlfAnderson <jackanderson78907890@gmail.com>
2025-02-04 13:52:58 +0300

    commit sprint-1 #3

--- app/src/main/java/com/example/matulemain/presentation/onBoarding/onBoardingScreen.kt
+++ app/src/main/java/com/example/matulemain/presentation/onBoarding/onBoardingScreen.kt
@@ -1,36 +1,47 @@
 package com.example.matulemain.presentation.onBoarding
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.matulemain.R
-import com.example.matulemain.ui.theme.accent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Composable
 fun OnBoardingScreen() {
@@ -52,18 +63,96 @@ fun OnBoardingScreen() {
             "В вашей комнате много красивых и привлекательных растений"
         )
     )
-
     var pagerState = rememberPagerState { pages.size }
 
-    HorizontalPager(pagerState) { page ->
-        Box (Modifier.fillMaxSize()){
-            if(page == 0){
-                OnBoardFirstScreen()
-            } else {
-                OnBoardOtherScreen(pages[page])
+    Box(Modifier.fillMaxSize()) {
+        HorizontalPager(pagerState) { page ->
+            val alpha by animateFloatAsState(
+                if (pagerState.currentPage == page) 1f else 0f,
+                tween(300)
+            )
+            Box(
+                Modifier
+                    .fillMaxSize()
+
+            ) {
+                if (page == 0) {
+                    OnBoardFirstScreen()
+                } else {
+                    OnBoardOtherScreen(pages[page])
+                }
             }
         }
     }
+    val scope = rememberCoroutineScope()
+
+    //Button
+    Box(
+        Modifier.fillMaxSize(),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        Button(
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.White,
+            ),
+            onClick = {
+                scope.launch {
+                    if(pagerState.currentPage != 2){
+                        pagerState.animateScrollToPage(pagerState.currentPage + 1)
+                    }else{
+                        //navigateplease
+                    }
+
+                }
+
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 20.dp, end = 20.dp, bottom = 36.dp)
+                .height(50.dp),
+            shape = RoundedCornerShape(13.dp),
+        ) {
+            Text(if (pagerState.currentPage == 0) "Начать" else "Далее", color = Color.Black)
+        }
+    }
+
+    val paddingIdicator by animateDpAsState(
+        if (pagerState.currentPage == 0) 212.dp else 171.dp,
+        tween(300)
+    )
+
+    Box(
+        Modifier
+            .padding(bottom = paddingIdicator)
+            .fillMaxSize(), contentAlignment = Alignment.BottomCenter
+    ) {
+        Row {
+            repeat(pagerState.pageCount, {
+                val animatePager by animateDpAsState(
+                    if (pagerState.currentPage == it) 43.dp else 28.dp,
+                    tween(300)
+                )
+                val animateColor by animateColorAsState(
+                    if (pagerState.currentPage == it) Color.White else Color(0xFF2B6B8B),
+                    tween(300)
+                )
+
+                Box(
+                    modifier = Modifier
+                        .padding(horizontal = 6.dp)
+                        .clip(CircleShape)
+                        .background(animateColor)
+                        .height(5.dp)
+                        .width(animatePager)
+
+                ) {
+
+                }
+            })
+        }
+
+    }
+
 
 }
 
@@ -99,27 +188,6 @@ fun OnBoardOtherScreen(onBoardItem: OnBoardItem) {
         )
         Spacer(Modifier.height(135.dp))
     }
-    Box(
-        Modifier.fillMaxSize(),
-        contentAlignment = Alignment.BottomCenter
-    ) {
-        Button(
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color.White,
-            ),
-            onClick = {
-
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 20.dp, end = 20.dp, bottom = 36.dp)
-                .height(50.dp),
-            shape = RoundedCornerShape(13.dp),
-        ) {
-            Text("Далее", color = Color.Black)
-        }
-    }
-
 
 }
 
@@ -145,10 +213,18 @@ fun OnBoardFirstScreen() {
                 Modifier
                     .fillMaxWidth()
                     .height(400.dp)
-            )
-        }
-    }
 
+            )
+
+        }
+
+    }
+}
+
+@Preview
+@Composable
+private fun OnBoardScreensPreview() {
+    OnBoardingScreen()
 }
 
 @Preview
